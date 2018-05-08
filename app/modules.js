05b5e42c432a76621ccb76d13599d6d2535aa07f
Ozmar Ugarte <ozmar_11@hotmail.com>
2018-05-08 14:36:12 -0500

    Greening the module specs

--- app/modules.js
+++ app/modules.js
@@ -1,7 +1,15 @@
 exports = typeof window === 'undefined' ? global : window;
 
+function Greeting(greeting, name) {
+	this.greeting = greeting;
+	this.name = name;
+	this.sayIt =  function () {
+		return this.greeting+', '+this.name;
+	}
+}
+
 exports.modulesAnswers = {
   createModule: function(str1, str2) {
-
+  	return new Greeting(str1, str2);
   }
 };
