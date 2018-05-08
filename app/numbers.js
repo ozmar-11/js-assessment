f36fa5bf6270d0a08ae247ce7e36819f4695159f
Ozmar Ugarte <ozmar_11@hotmail.com>
2018-05-08 14:36:48 -0500

    Greening the numbers specs

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,25 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+  	var binaryString = (num).toString(2);
+  	return parseInt(binaryString[binaryString.length - bit]);
   },
 
   base10: function(str) {
-
+  	return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
-
+  	var binaryString = (num).toString(2);
+  	var freeSlots = 8 - binaryString.length;
+  	for(var i = 0; i < freeSlots; i++) {
+  		binaryString = '0'+binaryString;
+  	}
+  	return binaryString;
   },
 
   multiply: function(a, b) {
-
+  	var precisionDecimals = (b.toString().split('.')[1] || []).length
+  	return parseFloat((a*b).toFixed(precisionDecimals));
   }
 };
